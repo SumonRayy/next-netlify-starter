a5e4d79e9741c34afd0405570823efd095330755
SumonRayy <sumonrayy@gmail.com>
2022-09-14 20:00:28 +0530

    🧹 template clean-up & ⚙️ env setup

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,35 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+const resumeLink = process.env.NEXT_PUBLIC_RESUME_LINK;
+const firstName = process.env.NEXT_PUBLIC_FIRST_NAME;
+const lastName = process.env.NEXT_PUBLIC_LAST_NAME;
+const aboutMe = process.env.NEXT_PUBLIC_ABOUT_ME;
+const profilePic = process.env.NEXT_PUBLIC_PROFILE_PIC;
+
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>I am {firstName} 🙂 </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <img src={profilePic} alt="Profile Picture" className="profilePic" />
+        <h1>
+          Hi! Welcome to My Website... 👋🙂
+        </h1>
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          I am<code>{firstName} {lastName}</code>
+          <br />
+          {aboutMe}
         </p>
+        <div>
+          <button className="btn"
+            onClick={() => window.open(resumeLink, "_blank")}
+          >Download my resume</button>
+        </div>
       </main>
 
       <Footer />
